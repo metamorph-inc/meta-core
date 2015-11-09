450be864f21e2d947a21ce4b37f40ba3e5359828
Kevin Smyth <kevin.m.smyth@gmail.com>
2015-11-09 14:09:34 -0600

    Upgrade to VS2015

--- jenkins_build.cmd
+++ jenkins_build.cmd
@@ -4,7 +4,7 @@ pushd %~dp0
 
 where UdmDll_3_2_VS10.dll
 
-c:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild make.msbuild /t:Installer /fl /flp:Verbosity=diag;PerformanceSummary /m /nodeReuse:false
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild make.msbuild /t:Installer /fl /flp:Verbosity=diag;PerformanceSummary /m /nodeReuse:false /p:"VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140"\;PlatformToolset=v140
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild make.msbuild /t:source_code_analysis /fl /flp:Verbosity=diag;PerformanceSummary;LogFile=source_code_analysis.log /m /nodeReuse:false
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
