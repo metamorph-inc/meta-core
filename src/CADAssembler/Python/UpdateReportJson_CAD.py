18f7fe01991cdeab35dc81861fc784446cacdc3a
Kevin Smyth <kevin.m.smyth@gmail.com>
2016-04-20 14:41:26 -0500

    CAD testbench_manifest: dont fail when non-temperature units are specified

--- src/CADAssembler/Python/UpdateReportJson_CAD.py
+++ src/CADAssembler/Python/UpdateReportJson_CAD.py
@@ -1,9 +1,6 @@
 import json
 import os
-import sys
-from xml.etree.ElementTree import ElementTree
 import ComputedMetricsSummary
-import utility_functions
 import logging
 
 
@@ -11,28 +8,21 @@ def add_options(parser):
     parser.add_option('-m', '--metricfile', help='input metrics file')
     (opts, args) = parser.parse_args()
     return opts
-    
+
 
 def convert_pp_metric(metric, value, inUnit, outUnit):
-    # If GME testbench metrics have either degrees Celsius or Fahrenheit,
-    # convert data from Kelvin to desired units.    
-    logger = logging.getLogger()
+    """Convert temperature to outUnit, otherwise returns None."""
+    logger = logging.getLogger(__name__)
     logger.info('Converting ' + metric + ' of ' + str(value) + ' ' + inUnit + ' to ' + outUnit)
     if str(outUnit) == 'Degree Celsius':
         return float(float(value) - 273.15)
     elif str(outUnit) == 'Degree Fahrenheit':
-        return float(1.8*(float(value) - 273.15) + 32)   
-    elif str(outUnit) == 'MPa':
-        # Place holder for now
-        return value
-    
+        return float(1.8*(float(value) - 273.15) + 32)
+    return None
+
 
 def update_manifest(tbmanifest, computedValuesXml):
-    #from optparse import OptionParser
-    #parser = OptionParser()
-    #options = add_options(parser)
-
-    logger = logging.getLogger()
+    logger = logging.getLogger(__name__)
     logger.info('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     logger.info('FUNCTION: UpdateReportJson_CAD()\n')
 
@@ -46,10 +36,10 @@ def update_manifest(tbmanifest, computedValuesXml):
             if os.path.exists('_FAILED.txt'):
                 result_json['Status'] = 'FAILED'
             else:
-                    result_json['Status'] = 'OK'
+                result_json['Status'] = 'OK'
         else:
             logger.debug('%s does not contain Status' % tbmanifest)
-        
+
         if os.path.exists(computedValuesXml):
             ComputedMetricsSummary.ParseReqMetricsFile(computedValuesXml)
 
@@ -60,11 +50,12 @@ def update_manifest(tbmanifest, computedValuesXml):
                         if key in ComputedMetricsSummary.gMetricSummary:
                             # update metric's value to the last value in
                             # time series
-                            if metric['Unit'] != '':
+                            metric['Value'] = None
+                            if metric.get('Unit') is not None and metric['Unit'] != '':
                                 metric['Value'] = convert_pp_metric(key, ComputedMetricsSummary.gMetricSummary[key][1],
                                                                     ComputedMetricsSummary.gMetricSummary[key][0],
                                                                     metric['Unit'])
-                            else:
+                            if metric['Value'] is None:
                                 metric['Value'] = ComputedMetricsSummary.gMetricSummary[key][1]
                                 metric['Unit'] = ComputedMetricsSummary.gMetricSummary[key][0]
                             logger.debug('Metric: {0} {1} {2} was updated.'.format(metric['Name'],
@@ -77,15 +68,16 @@ def update_manifest(tbmanifest, computedValuesXml):
                 # create warning message
                 logger.debug('% does not contain Metrics' % tbmanifest)
         else:
-            logger.debug('Given result file does not exist: {0}'.format(computedValuesXml))
+            logger.error('Given result file does not exist: {0}'.format(computedValuesXml))
 
         # update json file with the new values
         with open(tbmanifest, 'wb') as file_out:
             json.dump(result_json, file_out, indent=4)
         logger.debug('Finished updating %s file.' % tbmanifest)
-        
+
     else:
-        logger.debug('%s does not exist!' % tbmanifest)
+        logger.error('%s does not exist!' % tbmanifest)
+
 
     
 """
@@ -243,6 +235,7 @@ def update_manifest_per_interference_check(in_tbmanifest, in_interference_file):
 
 
 if __name__ == '__main__':
+    # logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
     manifest = os.path.join(os.getcwd(), 'testbench_manifest.json')
     update_manifest(manifest, 'ComputedValues.xml')
     interference_check_file = os.path.join(os.getcwd(), 'CADAssembly_interference.txt')
