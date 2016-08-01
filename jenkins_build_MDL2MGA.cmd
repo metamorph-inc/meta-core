6cc2b643631a0c8c9562f85499bcce956154958e
Kevin Smyth <kevin.m.smyth@gmail.com>
2016-08-01 15:54:03 -0500

    Dont store symbols

--- jenkins_build_MDL2MGA.cmd
+++ jenkins_build_MDL2MGA.cmd
@@ -6,18 +6,3 @@ where UdmDll_3_2_VS10.dll
 
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" make_MDL2MGA.msbuild /t:All;Push_All_NuGet /fl /flp:diag;PerformanceSummary /m /nodeReuse:false
 IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-set DBGTOOLS=%ProgramFiles%\Debugging Tools for Windows (x86)
-IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" set DBGTOOLS=%ProgramFiles%\Debugging Tools for Windows (x64)
-IF "%PROCESSOR_ARCHITEW6432%" == "AMD64" set DBGTOOLS=%ProgramW6432%\Debugging Tools for Windows (x64)
-IF NOT EXIST "%DBGTOOLS%" set DBGTOOLS=%ProgramFiles(x86)%\Debugging Tools for Windows (x86)
-
-echo %TIME%
-pushd externals\HCDDES
-call "%DBGTOOLS%\srcsrv\svnindex.cmd" /debug /Ini="%CD%\..\common-scripts\srcsrv.ini" /source="%CD%" /symbols="%CD%"
-popd
-IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-echo %TIME%
-copy_pdbs.py
-IF %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-echo %TIME%
