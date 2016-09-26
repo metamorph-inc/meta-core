1d5398b9a1198cd7041f4039d10b7c97a852ec2d
Kevin Smyth <kevin.m.smyth@gmail.com>
2016-09-26 10:22:45 -0500

    MetaLink dialog: fix tab order, button placement

--- src/CyPhyMetaLink/CreateOrEditComponent.Designer.cs
+++ src/CyPhyMetaLink/CreateOrEditComponent.Designer.cs
@@ -35,33 +35,31 @@
             // 
             // btnCreateNewComponent
             // 
-            this.btnCreateNewComponent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCreateNewComponent.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnCreateNewComponent.Location = new System.Drawing.Point(15, 58);
+            this.btnCreateNewComponent.Location = new System.Drawing.Point(12, 68);
             this.btnCreateNewComponent.Name = "btnCreateNewComponent";
             this.btnCreateNewComponent.Size = new System.Drawing.Size(301, 48);
-            this.btnCreateNewComponent.TabIndex = 7;
+            this.btnCreateNewComponent.TabIndex = 2;
             this.btnCreateNewComponent.Text = "Copy component and open in Creo";
             this.btnCreateNewComponent.UseVisualStyleBackColor = true;
             // 
             // btnEditExistingComponent
             // 
             this.btnEditExistingComponent.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnEditExistingComponent.Location = new System.Drawing.Point(15, 12);
+            this.btnEditExistingComponent.Location = new System.Drawing.Point(12, 12);
             this.btnEditExistingComponent.Name = "btnEditExistingComponent";
             this.btnEditExistingComponent.Size = new System.Drawing.Size(301, 48);
-            this.btnEditExistingComponent.TabIndex = 8;
+            this.btnEditExistingComponent.TabIndex = 1;
             this.btnEditExistingComponent.Text = "Open existing component in Creo";
             this.btnEditExistingComponent.UseVisualStyleBackColor = true;
             // 
             // btnEmptyCreoOpen
             // 
             this.btnEmptyCreoOpen.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnEmptyCreoOpen.Location = new System.Drawing.Point(15, 109);
+            this.btnEmptyCreoOpen.Location = new System.Drawing.Point(12, 122);
             this.btnEmptyCreoOpen.Name = "btnEmptyCreoOpen";
             this.btnEmptyCreoOpen.Size = new System.Drawing.Size(301, 48);
-            this.btnEmptyCreoOpen.TabIndex = 9;
+            this.btnEmptyCreoOpen.TabIndex = 3;
             this.btnEmptyCreoOpen.Text = "Open Creo without a component";
             this.btnEmptyCreoOpen.UseVisualStyleBackColor = true;
             // 
@@ -69,7 +67,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(329, 169);
+            this.ClientSize = new System.Drawing.Size(329, 186);
             this.Controls.Add(this.btnEmptyCreoOpen);
             this.Controls.Add(this.btnEditExistingComponent);
             this.Controls.Add(this.btnCreateNewComponent);
