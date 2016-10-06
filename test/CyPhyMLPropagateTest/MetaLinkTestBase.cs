b88cf8c6c1d2e2f75f088b07f7899a0ac4d77738
Kevin Smyth <kevin.m.smyth@gmail.com>
2016-10-06 13:46:59 -0500

    MetaLink: look for JDK in addition to JRE

--- test/CyPhyMLPropagateTest/MetaLinkTestBase.cs
+++ test/CyPhyMLPropagateTest/MetaLinkTestBase.cs
@@ -216,33 +216,29 @@ namespace CyPhyPropagateTest
 
         private string GetJavaInstallationPath()
         {
-            String javaKey = "SOFTWARE\\JavaSoft\\Java Runtime Environment";
-            try
+
+            foreach (var javaKey in new[] {
+                new { key = "SOFTWARE\\JavaSoft\\Java Runtime Environment", view = RegistryView.Registry64 },
+                new { key = "SOFTWARE\\JavaSoft\\Java Runtime Environment", view = RegistryView.Registry32 },
+                new { key = "SOFTWARE\\JavaSoft\\Java Development Kit", view = RegistryView.Registry64 },
+                new { key = "SOFTWARE\\JavaSoft\\Java Development Kit", view = RegistryView.Registry32 },
+            })
             {
-                using (var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(javaKey))
+                try
                 {
-                    String currentVersion = baseKey.GetValue("CurrentVersion").ToString();
-                    using (var homeKey = baseKey.OpenSubKey(currentVersion))
-                        return homeKey.GetValue("JavaHome").ToString();
+                    using (var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, javaKey.view).OpenSubKey(javaKey.key))
+                    {
+                        String currentVersion = baseKey.GetValue("CurrentVersion").ToString();
+                        using (var homeKey = baseKey.OpenSubKey(currentVersion))
+                            return homeKey.GetValue("JavaHome").ToString();
+                    }
+                }
+                catch (Exception)
+                {
+                    // no 64-bit Java was found. Will try 64-bit
                 }
             }
-            catch (Exception)
-            {
-                // no 64-bit Java was found. Will try 64-bit
-            }
-            try
-            {
-                using (var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(javaKey))
-                {
-                    String currentVersion = baseKey.GetValue("CurrentVersion").ToString();
-                    using (var homeKey = baseKey.OpenSubKey(currentVersion))
-                        return homeKey.GetValue("JavaHome").ToString();
-                }
-            }
-            catch (Exception)
-            {
-                // no success with 32-bit version either, will return null
-            }
+
             return null;
         }
 
