0aac2837d50963d0895f10594192f68d8ad774e8
Kevin Smyth <kevin.m.smyth@gmail.com>
2017-04-25 12:52:01 -0500

    Update run_mdao

--- src/Python27Packages/run_mdao/python_component/__init__.py
+++ src/Python27Packages/run_mdao/python_component/__init__.py
@@ -10,11 +10,16 @@ from openmdao.core.component import Component
 
 
 def PythonComponent(filename):
-    modname = os.path.splitext(os.path.basename(filename))[0]
-    py_suffix = [suffix for suffix in imp.get_suffixes() if suffix[0] == os.path.splitext(filename)[1]][0]
-    with open(filename, py_suffix[1]) as modfile:
-        # n.b. "If the module was already imported, it is equivalent to a reload()!"
-        mod = imp.load_module(modname, modfile, filename, py_suffix)
+    # support loading adjacent .py files as dependencies
+    sys.path.append(os.path.dirname(os.path.abspath(filename)))
+    try:
+        modname = os.path.splitext(os.path.basename(filename))[0]
+        py_suffix = [suffix for suffix in imp.get_suffixes() if suffix[0] == os.path.splitext(filename)[1]][0]
+        with open(filename, py_suffix[1]) as modfile:
+            # n.b. "If the module was already imported, it is equivalent to a reload()!"
+            mod = imp.load_module(modname, modfile, filename, py_suffix)
+    finally:
+        sys.path.pop()
 
     def is_component(val):
         return isinstance(val, type) and val.__module__ == modname and Component in val.__mro__
