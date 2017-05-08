d000e00f6260d66035521fadba8f51f714e8f882
Kevin Smyth <kevin.m.smyth@gmail.com>
2017-05-08 13:45:37 -0500

    Update run_mdao. OPENMETA-92

--- src/Python27Packages/run_mdao/csv_recorder.py
+++ src/Python27Packages/run_mdao/csv_recorder.py
@@ -1,4 +1,8 @@
 from __future__ import absolute_import
+import os
+import os.path
+import shutil
+import errno
 import sys
 import csv
 import uuid
@@ -8,6 +12,7 @@ from collections import OrderedDict
 import numpy
 
 from openmdao.recorders.base_recorder import BaseRecorder
+from openmdao.api import FileRef
 from six.moves import map
 
 
@@ -49,6 +54,7 @@ class CsvRecorder(BaseRecorder):
 
 
 class MappingCsvRecorder(BaseRecorder):
+    artifacts_directory = 'artifacts'
 
     def __init__(self, params_map, unknowns_map, out=sys.stdout, include_id=False):
         super(MappingCsvRecorder, self).__init__()
@@ -72,21 +78,39 @@ class MappingCsvRecorder(BaseRecorder):
             id = []
             if self._include_id:
                 id = ['GUID']
-            self.writer.writerow(id + list(self.params_map.values()) + list(self.unknowns_map.values()))
+            self.writer.writerow(id + list(self.params_map.values()) +
+                [v for k, v in six.iteritems(self.unknowns_map) if isinstance(unknowns[k], FileRef) is False])
             self._wrote_header = True
+
+        id = []
+        if self._include_id:
+            id = [uuid.uuid4()]
 
         def munge(val):
             if isinstance(val, numpy.ndarray):
                 return str(val.tolist())
+            if isinstance(val, FileRef):
+                return None
             return str(val)
 
         def do_mapping(map_, values):
-            return [munge(values[key]) for key in map_]
-        # import pdb; pdb.set_trace()
-        id = []
-        if self._include_id:
-            id = [uuid.uuid4()]
+            return [v for v in (munge(values[key]) for key in map_) if v is not None]
         self.writer.writerow(id + do_mapping(self.params_map, params) + do_mapping(self.unknowns_map, unknowns))
+
+        if self._include_id:
+            # write FileRefs to artifacts/GUID/mapped_name
+            for name, fileref in ((n, fr) for n, fr in six.iteritems(dict(unknowns)) if isinstance(fr, FileRef)):
+                with fileref.open('rb') as artifact:
+                    directory = os.path.join(self.artifacts_directory, six.text_type(id[0]))
+                    try:
+                        os.makedirs(directory)
+                    except OSError as e:
+                        if e.errno == errno.EEXIST and os.path.isdir(directory):
+                            pass
+                        else:
+                            raise
+                    with open(os.path.join(directory, self.unknowns_map[name]), 'wb') as out:
+                        shutil.copyfileobj(artifact, out)
 
         if self.out:
             self.out.flush()
--- src/Python27Packages/run_mdao/test/__init__.py
+++ src/Python27Packages/run_mdao/test/__init__.py
@@ -7,7 +7,9 @@ import os
 import os.path
 import shutil
 import subprocess
+import unittest
 import contextlib
+import csv
 
 _this_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -22,26 +24,54 @@ def run_regression(output_filename):
         raise Exception(changed)
 
 
-def test_main():
-    tb_json_names = glob.glob(os.path.join(_this_dir, '*/testbench_manifest.json'))
-    tb_jsons = [(tb_json_name, open(tb_json_name, 'r').read()) for tb_json_name in tb_json_names]
-    try:
-        for input_filename in glob.glob(_this_dir + '/mdao_config*json'):
-            output_filename = input_filename + '.output.csv'
-            with run_regression(output_filename):
-                run_mdao.run(input_filename)
+class RegressionTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        tb_json_names = glob.glob(os.path.join(_this_dir, '*/testbench_manifest.json'))
+        cls.tb_jsons = [(tb_json_name, open(tb_json_name, 'r').read()) for tb_json_name in tb_json_names]
 
-        with run_regression(os.path.join(_this_dir, 'single_run.csv')):
-            run_mdao.run_one('mdao_config_constant.json', (('designVariable.Naca_Code', 4040), ))
-
-        with run_regression(os.path.join(_this_dir, 'run_failure.csv')):
-            run_mdao.run_one('mdao_config_basic_CyPhy.json', (('designVariable.y', 0), ('designVariable.x', 'Ia')))
-
-    finally:
-        for tb_json_name, contents in tb_jsons:
+    @classmethod
+    def tearDownClass(cls):
+        for tb_json_name, contents in cls.tb_jsons:
             with open(tb_json_name, 'w') as tb_json:
                 tb_json.write(contents)
 
+    def _test_mdao_config(self, input_filename, output_filename):
+        with run_regression(output_filename):
+            run_mdao.run(input_filename)
+
+    def test_single_run(self):
+        with run_regression(os.path.join(_this_dir, 'single_run.csv')):
+            run_mdao.run_one('mdao_config_constant.json', (('designVariable.Naca_Code', 4040), ))
+
+    def test_run_failure(self):
+        with run_regression(os.path.join(_this_dir, 'run_failure.csv')):
+            run_mdao.run_one('mdao_config_basic_CyPhy.json', (('designVariable.y', 0), ('designVariable.x', 'Ia')))
+
+    def test_mdao_config_artifacts(self):
+        os.chdir(_this_dir)
+        run_mdao.run('mdao_config_artifacts.json')
+        with open('output.csv') as output:
+            csvreader = iter(csv.reader(output))
+            headers = next(csvreader)
+            for row in csvreader:
+                guid = row[headers.index('GUID')]
+                self.assertTrue(os.path.isfile(os.path.join('artifacts', guid, 'bin.out')))
+
+for input_filename in glob.glob(_this_dir + '/mdao_config*json'):
+    output_filename = input_filename + '.output.csv'
+
+    def _add_test(input_filename, output_filename):
+        def test(self):
+            self._test_mdao_config(input_filename, output_filename)
+
+        name = 'test_' + os.path.splitext(os.path.basename(input_filename))[0]
+        if getattr(RegressionTest, name, None) is None:
+            test.__name__ = name
+            setattr(RegressionTest, name, test)
+
+    _add_test(input_filename, output_filename)
+
 
 if __name__ == '__main__':
-    test_main()
+    unittest.main()
