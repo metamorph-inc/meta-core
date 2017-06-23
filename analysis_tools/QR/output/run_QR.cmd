0b7ea9c12de03817839bb34790ed3518420a3d27
Kevin Smyth <kevin.m.smyth@gmail.com>
2017-06-23 15:24:10 -0500

    Remove analysis_tools/QR

--- analysis_tools/QR/output/run_QR.cmd
+++
@@ -1,3 +0,0 @@
-:: Formal verification using QR
-echo off
-FOR /F "skip=2 tokens=2,*" %%A IN ('%SystemRoot%\SysWoW64\REG.exe query "HKLM\software\META" /v "META_PATH"') DO "%%B\bin\Python27\Scripts\Python.exe" scripts\QR_runner.py
