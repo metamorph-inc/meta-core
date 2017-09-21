metamorph-inc/meta-core

commit ac9d3f6d44e5d2153f803e9277a99b2c491ce5f1

MetaLink: detect when Creo exits abnormally (e.g. xtop.exe is killed)

src/CADAssembler/CADCreoParametricMetaLink/CADCreoParametricMetaLink.cpp
@@ -15,6 +15,7 @@
 #include <ISISVersionNumber.h>
 #include "CADFactoryAbstract.h"
 #include "CADFactoryCreo.h"
+#include "CreoErrorCodes.h"
 #include <AssembleUtils.h>
 
 #include <boost/filesystem.hpp>
@@ -308,6 +309,13 @@ int main(int argc, char *argv[])
         while(!terminateProcess)
         {
             ProEventProcess();
+            ProErr err;
+            if ((err = ProEngineerStatusGet()) != PRO_TK_NO_ERROR)
+            {
+                exceptionErrorStringStream << "Creo exited abnormally: " << isis::ProToolKitError_string(err).c_str();
+                ExitCode = -4;
+                break;
+            }
             if(programInputArguments.inputXmlFileName.size()!=0 && !inputFileProcessed)
             {
                 metalink_handler.CreateAssembly(programInputArguments.inputXmlFileName);

.gitignore
@@ -246,6 +246,7 @@ models/ComponentsAndArchitectureTeam/ComponentLibraryManager_AddOn/
 models/ComponentsAndArchitectureTeam/FormulaEvaluator/FormulaEvaluator_test.mga
 models/ComponentsAndArchitectureTeam/FormulaEvaluator/log/
 models/DesignSpace/Refactorer_test.mga
+models/DesignSpace/ToyDS.mga
 models/DesignSpace/ToyDS_test.mga
 models/DesignSpace/log/
 models/DynamicsTeam/CLM_Light/CLM_Light_test.mga
@@ -579,6 +580,7 @@ test/InterchangeTest/DesignInterchangeTest/ImportTestUnits/ValueFlow/ValueFlow_t
 test/InterchangeTest/DesignInterchangeTest/ImportTestUnits/ValueFlow/log/
 test/nunit_results.xml
 test/results/
+test/ModelTest/temp/
 externals/desert/DesertBackCompare/ReleaseUnicode/
 externals/desert/ReleaseUnicode/
 models/DynamicsTeam/RISimpleFormula/NonScalarParamsProps_ParamCA/

models/CADTeam/.gitignore
@@ -1,0 +1,23 @@
+*.mga
+*.pyc
+/MetaLink_*
+CADCreoParametricCreateAssembly_err.log
+CyPhy2CAD.log
+components/**/*.acm
+components/**/_FAILED.txt
+components/**/config.pro
+components/**/std.out
+components/**/trail.txt.*
+config/
+dashboard/
+design-space/
+designs/
+export_for_dashboard_scoring.py
+index.html
+log/
+requirements/
+stats/
+test-benches/
+
+results/
+manifest.project.json

src/CADAssembler/CADCreoParametricCommonFunctions/CADCreoParametricCommonFunctions.vcxproj
@@ -81,7 +81,7 @@
     <ClCompile>
       <WarningLevel>Level3</WarningLevel>
       <Optimization>Disabled</Optimization>
-      <PreprocessorDefinitions>PRO_USE_VAR_ARGS;PRO_MACHINE=29;PRO_OS=4;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>PRO_USE_VAR_ARGS;PRO_MACHINE=29;PRO_OS=4;STRICT;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>
 $(CREO_INSTALL)/protoolkit/includes;
 $(CREO_INSTALL)/protoolkit/protk_appls/includes;
@@ -98,7 +98,7 @@ $(CREO_INSTALL)/protoolkit/protk_appls/includes;
       <PrecompiledHeader>NotUsing</PrecompiledHeader>
       <WarningLevel>Level3</WarningLevel>
       <Optimization>Disabled</Optimization>
-      <PreprocessorDefinitions>PRO_USE_VAR_ARGS;PRO_MACHINE=36;PRO_OS=4;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>PRO_USE_VAR_ARGS;PRO_MACHINE=36;PRO_OS=4;STRICT;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>
 $(CREO_INSTALL)/protoolkit/includes;
 $(CREO_INSTALL)/protoolkit/protk_appls/includes;
@@ -116,7 +116,7 @@ $(CREO_INSTALL)/protoolkit/protk_appls/includes;
       <Optimization>MaxSpeed</Optimization>
       <FunctionLevelLinking>true</FunctionLevelLinking>
       <IntrinsicFunctions>true</IntrinsicFunctions>
-      <PreprocessorDefinitions>PRO_USE_VAR_ARGS;PRO_MACHINE=29;PRO_OS=4;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>PRO_USE_VAR_ARGS;PRO_MACHINE=29;PRO_OS=4;STRICT;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>
 $(CREO_INSTALL)/protoolkit/includes;
 $(CREO_INSTALL)/protoolkit/protk_appls/includes;
@@ -136,7 +136,7 @@ $(CREO_INSTALL)/protoolkit/protk_appls/includes;
       <Optimization>MaxSpeed</Optimization>
       <FunctionLevelLinking>true</FunctionLevelLinking>
       <IntrinsicFunctions>false</IntrinsicFunctions>
-      <PreprocessorDefinitions>PRO_USE_VAR_ARGS;PRO_MACHINE=36;PRO_OS=4;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>PRO_USE_VAR_ARGS;PRO_MACHINE=36;PRO_OS=4;STRICT;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>$(SolutionDir)/CADCreoParametricToolkitFunctions;$(SolutionDir)/CADCreoUtils;$(CREO_INSTALL)/protoolkit/includes;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <WholeProgramOptimization>true</WholeProgramOptimization>
       <RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>

src/CADAssembler/CADCreoParametricCommonFunctions/CADSoftwareEnvirUtils.cpp
@@ -160,6 +160,7 @@ namespace isis
 		//////////////////////////////////////////////////////////////////
 		std::vector<std::string> subKeys_vec;
 		RetrieveRegistryListOfSubkeys( hKey, subKeys_vec );
+		RegCloseKey(hKey);
 
 		std::vector<std::string> versionNumber_vec;
 		for ( std::vector<std::string>::const_iterator i(subKeys_vec.begin()); i != subKeys_vec.end(); ++i )
@@ -207,6 +208,7 @@ namespace isis
 		/////////////////////////////////////////////////////////////////////////////////////////////
 		subKeys_vec.clear();
 		RetrieveRegistryListOfSubkeys( hKey, subKeys_vec );
+		RegCloseKey(hKey);
 
 		if ( subKeys_vec.size() == 0 )
 		{
@@ -259,6 +261,7 @@ namespace isis
 		// Get InstallDir
 		////////////////////////////////
 		std::string installDir = RetrieveRegistryStringValue( hKey, "InstallDir" );
+		RegCloseKey(hKey);
 
 		if ( commonFilesLocation.size() == 0 )
 		{

src/CyPhyMetaLink/CyPhyMetaLinkInterpreter.cs
@@ -389,6 +389,7 @@ namespace CyPhyMetaLink
                                         bridgeListening.SetResult(true);
                                     }
                                     log.WriteLine(e.Data);
+                                    // log.Flush();
                                 }
                             };
                             metalink.OutputDataReceived += handler;

commit 50777d0bda955400d9c68b8040a08a6bbef0ec7b

Fix normal MetaLink exit, broken in ac9d3f6

src/CADAssembler/CADCreoParametricMetaLink/CADCreoParametricMetaLink.cpp
@@ -306,13 +306,21 @@ int main(int argc, char *argv[])
 
         bool inputFileProcessed = false;
 
-        while(!terminateProcess)
+        while (true)
         {
             ProEventProcess();
+            if (terminateProcess)
+            {
+                break;
+            }
             ProErr err;
             if ((err = ProEngineerStatusGet()) != PRO_TK_NO_ERROR)
             {
-                exceptionErrorStringStream << "Creo exited abnormally: " << isis::ProToolKitError_string(err).c_str();
+				if (terminateProcess)
+				{
+					break;
+				}
+				exceptionErrorStringStream << "Creo exited abnormally: " << isis::ProToolKitError_string(err).c_str();
                 ExitCode = -4;
                 break;
             }
@@ -396,6 +404,13 @@ int main(int argc, char *argv[])
             std::cerr << std::endl << std::endl << exceptionErrorStringStream.str() << std::endl << std::endl;
         }
 
+    }
+    else
+    {
+        if(Logging_Set_Up)
+        {
+            isis_LOG(lg, isis_FILE, isis_INFO) << "Normal exit" << std::endl;
+        }
     }
 
 