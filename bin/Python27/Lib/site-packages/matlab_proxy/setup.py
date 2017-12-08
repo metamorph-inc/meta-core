72247792974708749f5a88de96c7f7f1bc695db0
Tim Thomas <tthomas@metamorphsoftware.com>
2017-12-08 10:55:55 -0600

    Merge in 'matlab_wrapper' (3b9832449) repository.

--- bin/Python27/Lib/site-packages/matlab_proxy/setup.py
+++ bin/Python27/Lib/site-packages/matlab_proxy/setup.py
@@ -1,3 +1,12 @@
+r"""
+Make a 64-bit matlab_proxy (for use with 32-bit Python).
+
+C:\Python27_x64\python -m virtualenv venv
+.\venv\Scripts\pip install -r requirements.txt
+.\venv\Scripts\python setup.py py2exe
+.\venv\Scripts\python setup.py bdist_wheel -p win32
+"""
+
 from setuptools import setup, find_packages
 import sys
 import shutil
@@ -8,10 +17,16 @@ import platform
 if sys.argv[1] == 'py2exe':
     import py2exe
 
+with open('MANIFEST.in', 'wb') as manifest:
+    if 'bdist_wheel' in sys.argv:
+        manifest.write('recursive-include matlab_proxy/dist_{} *\n'.format(platform.architecture()[0]))
+
 setup(
     name="matlab_proxy",
     version="0.1",
-    packages=find_packages(),
+    packages=['matlab_proxy'],
+    include_package_data=True,
+    zip_safe=False,
     options={"py2exe": {
                         # 'xref': True,
                         # 'unbuffered': True,
@@ -19,13 +34,13 @@ setup(
                         "excludes": """Tkinter _tkinter matlab matlab.engine site
                             _ctypes _hashlib _socket _ssl bz2 pyexpat select""".split(),
                         "includes": ["pkgutil", "importlib"],
-                        "dist_dir": 'dist_' + platform.architecture()[0]
+                        "dist_dir": 'matlab_proxy/dist_' + platform.architecture()[0]
 
                         }},
     zipfile="Python{}{}.zip".format(*sys.version_info[0:2]),
-    console=[{'script': "matlab_proxy.py"}],
+    console=[{'script': "matlab_proxy/__init__.py"}],
 )
 
 if sys.argv[1] == 'py2exe':
-    shutil.copyfile(sys.executable, os.path.join('dist_' + platform.architecture()[0], os.path.basename(sys.executable)))
-    os.unlink(os.path.join('dist_' + platform.architecture()[0], 'matlab_proxy.exe'))
+    shutil.copyfile(sys.executable, os.path.join('matlab_proxy/dist_' + platform.architecture()[0], os.path.basename(sys.executable)))
+    os.unlink(os.path.join('matlab_proxy/dist_' + platform.architecture()[0], '__init__.exe'))
---
+++ bin/Python27/Lib/site-packages/matlab_wrapper/test/bare_file.m
@@ -0,0 +1,17 @@
+% "bare file" i.e. no function definition. inputs and outputs are defined in comments
+%variable: output1 double output matlabName="output1"
+%variable : output2 double output units="m" matlabName="output2"
+%variable: output3 double[] output
+%variable: output4 string output
+% variable : output5 string[] output
+%variable: input1 double input
+%variable : input2 double input
+% variable: input3 double[] input format="double"
+%variable: input4 string input format="double"
+%variable: input5 string[] input format="double"
+
+output1 = input1 * 2
+output2 = input2 * 2
+output3 = input3 * 2
+output4 = input4
+output5 = input5
