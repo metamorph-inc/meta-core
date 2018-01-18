5f343b3cacd4091a580da8bf9cd6d86bbd907747
Kevin Smyth <kevin.m.smyth@gmail.com>
2018-01-18 09:00:07 -0600

    CyPhyPET: fix remote execution for Python,MATLAB,Excel wrappers. OPENMETA-363

--- src/CyPhyPET/Resources/zip.py
+++ src/CyPhyPET/Resources/zip.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python
 import zipfile
-import os
 import sys
+import io
+import os
 import os.path
+import json
+import six
 
 path_join = os.path.join
 if sys.platform == 'win32':
@@ -14,16 +16,35 @@ output_filename = 'source_data.zip'
 if os.path.exists(output_filename):
     os.remove(output_filename)
 
+
 with zipfile.ZipFile(output_filename, 'w', allowZip64=True) as z:
-    parent_dir_name = os.path.basename(os.getcwd())
-    os.chdir('..\\')
-    for dirpath,dirs,files in os.walk(parent_dir_name):
-        if '.git' in dirpath or '.svn' in dirpath:
-            continue        
+    parent_dir_name = os.path.basename(six.moves.getcwd())
+
+    with io.open('mdao_config.json', 'r', encoding='utf8') as config_file:
+        mdao_config = json.load(config_file)
+
+    for name, component in six.iteritems(mdao_config['components']):
+        types = {
+            'excel_wrapper.excel_wrapper.ExcelWrapper': 'excelFile',
+            'matlab_wrapper.MatlabWrapper': 'mFile',
+            'run_mdao.python_component.PythonComponent': 'filename',
+        }
+        detailsKey = types.get(component.get('type'))
+        if detailsKey is None:
+            continue
+        filename = component['details'][detailsKey]
+        destFilename = name + '/' + os.path.basename(filename)
+        z.write(filename, arcname=parent_dir_name + '/' + destFilename, compress_type=zipfile.ZIP_DEFLATED)
+        component['details'][detailsKey] = destFilename
+
+    with open('mdao_config.json', 'w') as config_file:
+        json.dump(mdao_config, config_file, indent=4)
+
+    os.chdir('..')
+    for dirpath, dirs, files in os.walk(parent_dir_name):
         for f in files:
-            if output_filename == f or f.endswith('.suo'):
+            if output_filename == f:
                 continue
             fn = path_join(dirpath, f)
-            #print fn
+            # print fn
             z.write(fn, arcname=os.path.join(dirpath, f), compress_type=zipfile.ZIP_DEFLATED)
-
