5fd3e56030f173859b348d305187f26d4c105b7f
Kevin Smyth <kevin.m.smyth@gmail.com>
2018-03-01 10:24:13 -0600

    Modelica exporter: fix race with omc.exe

--- src/Python27Packages/py_modelica_exporter/py_modelica_exporter/omc_session.py
+++ src/Python27Packages/py_modelica_exporter/py_modelica_exporter/omc_session.py
@@ -1,4 +1,36 @@
+# coding: utf-8
 __author__ = 'Zsolt'
+
+__license__ = """
+ This file is part of OpenModelica.
+
+ Copyright (c) 1998-CurrentYear, Vanderbilt University; Metamorph Inc; Open Source Modelica Consortium (OSMC),
+ c/o Linköpings universitet, Department of Computer and Information Science,
+ SE-58183 Linköping, Sweden.
+
+ All rights reserved.
+
+ THIS PROGRAM IS PROVIDED UNDER THE TERMS OF THE BSD NEW LICENSE OR THE
+ GPL VERSION 3 LICENSE OR THE OSMC PUBLIC LICENSE (OSMC-PL) VERSION 1.2.
+ ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES
+ RECIPIENT'S ACCEPTANCE OF THE OSMC PUBLIC LICENSE OR THE GPL VERSION 3,
+ ACCORDING TO RECIPIENTS CHOICE.
+
+ The OpenModelica software and the OSMC (Open Source Modelica Consortium)
+ Public License (OSMC-PL) are obtained from OSMC, either from the above
+ address, from the URLs: http://www.openmodelica.org or
+ http://www.ida.liu.se/projects/OpenModelica, and in the OpenModelica
+ distribution. GNU version 3 is obtained from:
+ http://www.gnu.org/copyleft/gpl.html. The New BSD License is obtained from:
+ http://www.opensource.org/licenses/BSD-3-Clause.
+
+ This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, EXCEPT AS
+ EXPRESSLY SET FORTH IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE
+ CONDITIONS OF OSMC-PL.
+
+ Version: 1.1
+"""
 
 import os
 import sys
@@ -92,7 +124,7 @@ class OMCSession(object):
                     self._start_server()
                 except Exception as ex:
                     self.logger.error("The OpenModelica compiler is missing in the System path, please install it")
-                    raise ex
+                    raise
 
     def _connect_to_omc(self):
         # import the skeletons for the global module
@@ -110,20 +142,17 @@ class OMCSession(object):
         self._ior_file = os.path.join(self._temp_dir, self._ior_file)
         self._omc_corba_uri = "file:///" + self._ior_file
         # See if the omc server is running
-        if os.path.isfile(self._ior_file):
-            self.logger.info("OMC Server is up and running at {0}".format(self._omc_corba_uri))
-        else:
-            attempts = 0
-            while True:
-                if not os.path.isfile(self._ior_file):
-                    time.sleep(0.25)
-                    attempts += 1
-                    if attempts == 10:
-                        self.logger.error("OMC Server is down. Please start it!")
-                        raise Exception
-                    else:
-                        self.logger.info("OMC Server is up and running at {0}".format(self._omc_corba_uri))
-                        break
+        attempts = 0
+        while True:
+            if not os.path.isfile(self._ior_file):
+                time.sleep(0.25)
+                attempts += 1
+                if attempts == 50:
+                    self.logger.error("OMC Server is down. Please start it!")
+                    raise Exception("OMC Server is down. Please start it!")
+            else:
+                self.logger.info("OMC Server is up and running at {0}".format(self._omc_corba_uri))
+                break
 
         #initialize the ORB with maximum size for the ORB set
         sys.argv.append("-ORBgiopMaxMsgSize")
@@ -142,7 +171,7 @@ class OMCSession(object):
         # Check if we are using the right object
         if self._omc is None:
             self.logger.error("Object reference is not valid")
-            raise Exception
+            raise Exception("Object reference is not valid")
 
     def __init__(self, readonly=False):
         self.readonly = readonly
@@ -240,7 +269,7 @@ class OMCSession(object):
                 res = self._omc.sendExpression(expression)
         except Exception as e:
             # self.logger.debug("Failed: {0}({1}, parsed={2})".format(question, opt, parsed))
-            raise e
+            raise
 
         # save response
         self.omc_cache[p] = res
