b23002d88ff3fe3fa8b765faba20eeb58c7f5ab0
Jonathon Williams <jwilliams@metamorphsoftware.com>
2018-09-06 13:20:58 -0500

    OPENMETA-524 Tolerate missing log files in testbenchexecutor report generator

--- src/Python27Packages/testbenchexecutor/tb_report_generator.py
+++ src/Python27Packages/testbenchexecutor/tb_report_generator.py
@@ -19,16 +19,20 @@ def generate_html_report(manifest_path, manifest_dict):
     for step in manifest_dict["Steps"]:
         logPath = step["LogFile"]
         if logPath != None:
-            with io.open(os.path.join(manifest_dir, logPath), "r", encoding="utf-8") as logread:
-                loglines = logread.readlines()
-                if len(loglines) > 10:
-                    header = 'Partial log:'
-                else:
-                    header = 'Log contents:'
-                excerpt = loglines[-10:]
+            try:
+                with io.open(os.path.join(manifest_dir, logPath), "r", encoding="utf-8") as logread:
+                    loglines = logread.readlines()
+                    if len(loglines) > 10:
+                        header = 'Partial log:'
+                    else:
+                        header = 'Log contents:'
+                    excerpt = loglines[-10:]
 
-            logFileExcerpts[logPath] = ''.join(excerpt)
-            logFileHeaders[logPath] = header
+                logFileExcerpts[logPath] = ''.join(excerpt)
+                logFileHeaders[logPath] = header
+            except IOError as err:
+                logFileExcerpts[logPath] = "Log file failed to load:\n\n{0}".format(err)
+                logFileHeaders[logPath] = "Log file missing:" 
 
     template = env.get_template("report.html")
 
