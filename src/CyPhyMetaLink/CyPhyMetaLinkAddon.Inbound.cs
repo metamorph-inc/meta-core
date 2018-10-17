metamorph-inc/meta-core

commit f072d7c36bced3c0aa8fd40cd9217b366b960ef6

MetaLink: prevent error message in empty mode

src/CyPhyMetaLink/CyPhyMetaLinkAddon.Inbound.cs
@@ -313,7 +313,7 @@ namespace CyPhyMetaLink
                         addon.Project.AbortTransaction();
                     }
                 }
-                else
+                else if (cdata.Type == SyncedComponentData.EditType.Component)
                 {
                     string designId = cdata.Id;
                     string xml;
@@ -334,6 +334,10 @@ namespace CyPhyMetaLink
                     {
                         GMEConsole.Warning.WriteLine("MetaLink: unknown assembly " + designId);
                     }
+                }
+                else
+                {
+                    // cdata.Type == SyncedComponentData.EditType.Empty
                 }
             }
             else

commit ec52a9b2a747662ee9445f387d8d560af9ba8556

Fix MetaLink in ComponentAssembly mode, broken since f072d7c36

src/CyPhyMetaLink/CyPhyMetaLinkAddon.Inbound.cs
@@ -313,7 +313,7 @@ namespace CyPhyMetaLink
                         addon.Project.AbortTransaction();
                     }
                 }
-                else if (cdata.Type == SyncedComponentData.EditType.Component)
+                else if (cdata.Type == SyncedComponentData.EditType.Design)
                 {
                     string designId = cdata.Id;
                     string xml;