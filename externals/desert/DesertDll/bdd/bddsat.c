metamorph-inc/meta-core

commit 9d3a583e98a76d31db79b1b2ab947076fbf0e5c0

Desert: fix "Design space too large" even when number of configs is reduced by constraints (TASCK-148)
Also fix bug where user can click on GME while progress bar is displayed (trashing the transaction)

Fix UI block after user clicks Cancel during desert operations

Fix float precision loss for property constraints

externals/desert/DesertDll/bdd/bddsat.c
@@ -272,6 +272,11 @@ void bdd_sat_f_step_mat(bdd_manager bddm, bdd f, int *a, int len,
 #endif
   cur_index = BDD_INDEX(bddm, f);
 
+  if (*rows > BDD_MAX_PATHS) {
+    // give up here, runtime will be too long (and OOM handling is not great)
+    return;
+  }
+
   b = (int *)malloc(sizeof(int)*(num_vars+1));
   c = (int *)malloc(sizeof(int)*(num_vars+1));
 

externals/desert/DesertDll/core/bddmain.cpp
@@ -133,7 +133,15 @@ int CBdd::Satisfy(CBdd& b, std::deque<boost::dynamic_bitset<>>& encVectors, int 
     int rows=0;
     int **mat = new int*[BDD_MAX_PATHS];
     bdd_sat_f_mat(manager, f, 0, length-1, mat, &rows);
-      if (rows > 0)
+	if (rows > BDD_MAX_PATHS) {
+		for (int i = 0; i < BDD_MAX_PATHS; i++)
+		{
+			free(mat[i]);
+		}
+		delete[] mat;
+		return DESIGN_SPACE_TOO_LARGE;
+	}
+    if (rows > 0)
     {
       for (int i=0; i<rows; i++)
       {

externals/desert/DesertDll/bdd/bdduser.h
@@ -9,6 +9,7 @@
 #include <wtypes.h>
 #include "mem/memuser.h"
 
+#define BDD_MAX_PATHS 100000
 
 #if defined(__STDC__)
 #define ARGS(args) args

externals/desert/DesertDll/core/coredefs.h
@@ -4,6 +4,8 @@
 #ifndef DEFS_H
 #define DEFS_H
 
+#define DESIGN_SPACE_TOO_LARGE -2
+
 #include <afxtempl.h>
 
 // classes
@@ -195,7 +197,6 @@ typedef enum
 //#define new DEBUG_NEW
 //#endif
 
-#define BDD_MAX_PATHS 100000
 #define MAX_CONFIGURATIONS 1000000         // how many configurations can we deal with
 
 #ifdef DESERT_EXPORTS

externals/desert/DesertDll/core/manager.cpp
@@ -53,7 +53,6 @@ static TCHAR *setNames[] =
 };
 
 #define MAX_LONG (long)(1 << 30)
-#define DESIGN_SPACE_TOO_LARGE -2
 
 CManager::
 CManager() : CCosmic()
@@ -1604,13 +1603,6 @@ long CManager::CalcRealNoOfConfigurations()
 {
 	if (generations.IsEmpty()) GenerateNextHierarchy();
 
-	double sizeOfDesignSpace = ComputeSize();
-	if (sizeOfDesignSpace > MAX_CONFIGURATIONS) {
-		// TODO: If design space is too large, check the prun.sat_frac()
-		// at the end of this function
-		return DESIGN_SPACE_TOO_LARGE;
-	}
-
 	GET_CURRENTGENERATION
 
 	// get design space and constraints corresponding to the current generation
@@ -1645,7 +1637,7 @@ long CManager::CalcRealNoOfConfigurations()
 		config = space->MaptoBdd(one, 0) && config;
 	}
 
-	CDynConstraintSet *toSet = new CDynConstraintSet(0);
+	std::unique_ptr<CDynConstraintSet> toSet(new CDynConstraintSet(0));
 	toSet->RemoveAll();
 
 	pos = consets.GetHeadPosition();
@@ -1683,19 +1675,25 @@ long CManager::CalcRealNoOfConfigurations()
 			{
 				CDynElement *root = roots.GetNext(pos);
 				std::deque<boost::dynamic_bitset<>> encVectors;
-				CBdd::Satisfy(prun, encVectors, NULL, NULL);
-
-				// Get rid of double-counting
-				for (auto it = encVectors.begin(); it != encVectors.end(); ++it)
-				{
-					auto& encVec = *it;
-
-					// convert the value to bdd, to write configuration
-					if ( root->NotRedundant(encVec) ) {
-						numCfgsInEachRoot++;
+				struct args {
+					CDynElement *root;
+					long& numCfgsInEachRoot;
+				} arg = { root, numCfgsInEachRoot };
+				auto callback = [](void *arg, const boost::dynamic_bitset<>& encVec) -> int {
+					args* a = (args*)arg;
+					if (a->root->NotRedundant(encVec))
+					{
+						a->numCfgsInEachRoot++;
 					}
+					return 0;
+				};
+				typedef int(*callback_t)(void *, const boost::dynamic_bitset<>&);
+				int count = CBdd::Satisfy(prun, encVectors, (callback_t)callback, &arg);
+				if (count == DESIGN_SPACE_TOO_LARGE) {
+					CBdd::Finit();
+					toSet->RemoveAll();
+					return DESIGN_SPACE_TOO_LARGE;
 				}
-
 			} //eo while(pos)
 
 			totalNoOfConfigs += numCfgsInEachRoot;
@@ -1706,7 +1704,6 @@ long CManager::CalcRealNoOfConfigurations()
 	  {
 		CBdd::Finit();
 		toSet->RemoveAll();
-		delete toSet;
 		  throw e;
 	  }
 
@@ -1714,7 +1711,6 @@ long CManager::CalcRealNoOfConfigurations()
 	CBdd::Finit();
 
 	toSet->RemoveAll();
-	delete toSet;
 
 	return totalNoOfConfigs;
 }

externals/desert/DesertDll/core/manager.h
@@ -5,6 +5,11 @@
 #ifndef MANAGER_H
 #define MANAGER_H
 
+#ifndef BDD_MAX_PATHS
+#define BDD_MAX_PATHS 100000
+#endif
+
+
 #include <string>
 #ifdef _UNICODE
 typedef std::wstring tstring;

externals/desert/DesertDll/core/utils.cpp
@@ -70,9 +70,19 @@ RemoveCRLF(CString& text)
 
 IMPLEMENT_DYNAMIC(CDesertException, CException)
 
-inline CDesertException::CDesertException()						{fatal = true;		what = NULL;};			
-inline CDesertException::CDesertException(bool _fatal)			{fatal = _fatal;	what = NULL;};
-inline CDesertException::CDesertException(const TCHAR * _what)	{fatal = true;		if (_what) what = new TCHAR[_tcslen(_what)+1];_tcscpy(what, _what);};
+inline CDesertException::CDesertException() { fatal = true;		what = NULL; constraint_name = NULL; }
+inline CDesertException::CDesertException(bool _fatal) { fatal = _fatal;	what = NULL; constraint_name = NULL; }
+inline CDesertException::CDesertException(const TCHAR * _what) {
+	fatal = true;
+	constraint_name = nullptr;
+	if (_what) {
+		what = new TCHAR[_tcslen(_what) + 1];
+		_tcscpy(what, _what);
+	}
+	else {
+		what = nullptr;
+	}
+}
 inline CDesertException::CDesertException(const TCHAR * _what, const TCHAR * _name)	
 {	
 	fatal = true;		
@@ -88,7 +98,18 @@ inline CDesertException::CDesertException(const TCHAR * _what, const TCHAR * _na
 	}
 };
 
-inline CDesertException::CDesertException(const CString _what)	{fatal = true;		if (_what.GetLength()) what = new TCHAR[_what.GetLength()+1];_tcscpy(what, (LPCTSTR)_what);};
+inline CDesertException::CDesertException(const CString _what) {
+	fatal = true;
+	constraint_name = nullptr;
+	if (_what.GetLength()) {
+		what = new TCHAR[_what.GetLength() + 1];
+		_tcscpy(what, (LPCTSTR)_what);
+	}
+	else {
+		what = nullptr;
+	}
+
+}
 inline CDesertException::CDesertException(const CString _what, const CString _name)	
 {	
 	fatal = true;		
@@ -97,14 +118,40 @@ inline CDesertException::CDesertException(const CString _what, const CString _na
 		what = new TCHAR[_what.GetLength()+1];
 		_tcscpy(what, (LPCTSTR)_what);
 	}
+	else {
+		what = nullptr;
+	}
 	if(_name) 
 	{
 		constraint_name=new TCHAR[_name.GetLength()+1];
 		_tcscpy(constraint_name, (LPCTSTR)_name);
 	}
+	else {
+		constraint_name = nullptr;
+	}
 };
-inline CDesertException::CDesertException(bool _fatal, const TCHAR * _what)	{fatal = _fatal;	if (_what) what = new TCHAR[_tcslen(_what)+1];_tcscpy(what, _what);};
-inline CDesertException::CDesertException(bool _fatal, const CString _what)	{fatal = _fatal;	if (_what.GetLength()) what = new TCHAR[_what.GetLength()+1];_tcscpy(what, (LPCTSTR)_what);};
+inline CDesertException::CDesertException(bool _fatal, const TCHAR * _what) {
+	fatal = _fatal;
+	constraint_name = nullptr;
+	if (_what) {
+		what = new TCHAR[_tcslen(_what) + 1];
+		_tcscpy(what, _what);
+	}
+	else {
+		what = nullptr;
+	}
+}
+inline CDesertException::CDesertException(bool _fatal, const CString _what) {
+	fatal = _fatal;
+	constraint_name = nullptr;
+	if (_what.GetLength()) {
+		what = new TCHAR[_what.GetLength() + 1];
+		_tcscpy(what, (LPCTSTR)_what);
+	}
+	else {
+		what = nullptr;
+	}
+}
 
 inline BOOL CDesertException::GetErrorMessage( LPTSTR lpszError, UINT nMaxError, PUINT pnHelpContext)	
 {

externals/desert/DesertDll/desertdll.h
@@ -7,6 +7,8 @@
 
 #include "BackIface.h"
 
+#define DESIGN_SPACE_TOO_LARGE -2
+
 //error handling with exceptions
 
 class CDesertException : public CException

commit af981c928e504cbe996b05ebace8194736492b36

Fix desert crash introduced in 9d3a583e

externals/desert/DesertDll/bdd/bddsat.c
@@ -272,7 +272,7 @@ void bdd_sat_f_step_mat(bdd_manager bddm, bdd f, int *a, int len,
 #endif
   cur_index = BDD_INDEX(bddm, f);
 
-  if (*rows > BDD_MAX_PATHS) {
+  if (*rows >= BDD_MAX_PATHS) {
     // give up here, runtime will be too long (and OOM handling is not great)
     return;
   }

externals/desert/DesertDll/core/bddmain.cpp
@@ -133,7 +133,7 @@ int CBdd::Satisfy(CBdd& b, std::deque<boost::dynamic_bitset<>>& encVectors, int 
     int rows=0;
     int **mat = new int*[BDD_MAX_PATHS];
     bdd_sat_f_mat(manager, f, 0, length-1, mat, &rows);
-	if (rows > BDD_MAX_PATHS) {
+	if (rows >= BDD_MAX_PATHS) {
 		for (int i = 0; i < BDD_MAX_PATHS; i++)
 		{
 			free(mat[i]);